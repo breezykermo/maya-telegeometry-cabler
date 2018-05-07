61ee0671b9bcfbf2d35fc59681c1200279924e0b
Lachlan Kermode <lachiekermode@gmail.com>
2018-05-07 10:57:28 -0400

    clean examples

--- example.py
+++ example.py
@@ -3,14 +3,17 @@ importlib.import_module('lib/Cabler')
 
 RADIUS = 100
 cabler = Cabler(RADIUS)
-# cabler.tube('tata-tgn-atlantic')
-# def all():
-#   cable_ids = map(lambda d: d['id'], cabler.all)
-#   # cable_ids = map(lambda x: x['id'], cables)
-#   print cable_ids
-  # for cable_id in cable_ids:
-  #   cabler.tube(cable_id)
 
+# create cables for the TATA-TGN Atlantic network
+cabler.tube('tata-tgn-atlantic')
+
+# a function to populate a globe with all cables
+def all_cables():
+  cable_ids = map(lambda d: d['id'], cabler.all)
+  for cable_id in cable_ids:
+    cabler.tube(cable_id)
+
+# create a custom cable using lat/lon coords
 cabler.tube_from_points([
   LatLon(40.152908, -74.062861), # wall township, NJ
   LatLon(35.32533, -81.86866)    # forest city, NC
